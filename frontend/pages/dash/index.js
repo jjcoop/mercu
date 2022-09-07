2b1b20bb8ed8106164d882cea3d159afd2dc274f
damonDevelops <76411682+damonDevelops@users.noreply.github.com>
2022-09-07 13:50:58 +1000

    Delete frontend directory

--- frontend/pages/dash/index.js
+++
@@ -1,8 +0,0 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import Dashboard from "../../components/Dashboard/Dashboard";
-
-export default function dash() {
-  return <Dashboard />;
-}
