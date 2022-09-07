34b837f62dfe0c901af8108c33aa1e3656d382f6
damonDevelops <76411682+damonDevelops@users.noreply.github.com>
2022-09-07 13:58:02 +1000

    Add files via upload

---
+++ frontend/pages/procurement/index.js
@@ -0,0 +1,11 @@
+import BasicForm from "../../components/Dashboard/BasicForm";
+import Procure from "../../components/Procurement/Procurements";
+
+export default function procurement() {
+  return (
+
+        <div>
+          <Procure/>
+        </div>
+  );
+}
