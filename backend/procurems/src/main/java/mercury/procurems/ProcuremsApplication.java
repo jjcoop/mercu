b0dcad3ca709d18cada8942a558880003fc4a08b
Damon O'Neil <don988@uowmail.edu.au>
2022-09-09 15:32:27 +1000

    refactor:procuremsApplication.java - removed impor

--- backend/procurems/src/main/java/mercury/procurems/ProcuremsApplication.java
+++ backend/procurems/src/main/java/mercury/procurems/ProcuremsApplication.java
@@ -1,17 +1,11 @@
 package mercury.procurems;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class ProcuremsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProcuremsApplication.class, args);
 	}
-	
-
-	
 }
