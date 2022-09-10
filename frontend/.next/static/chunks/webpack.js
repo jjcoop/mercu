7c7d2229bcfc7984031dc6981ea3f93e1bf98d38
Damon O'Neil <don988@uowmail.edu.au>
2022-09-10 14:01:16 +1000

    update:integrated backend functionality

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8f908ffc3d2fff85"; }
+/******/ 		__webpack_require__.h = function() { return "cfb939f306fbd659"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ frontend/components/Procurement/CreateContact.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import SendIcon from "@mui/icons-material/Send";
+import Button from "@mui/material/Button";
+
+export default function CreateSupplierForm() {
+  const [myValue, setValue] = useState("");
+
+  // Handles the submit event on form submit.
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+
+    // Get data from the form.
+    const data = {
+      companyName: event.target.companyName.value,
+      base: event.target.base.value,
+    };
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data);
+
+    // API endpoint where we send form data.
+    const endpoint = "http://localhost:8787/supplierProcurement";
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: "POST",
+      // Tell the server we're sending JSON.
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options);
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json();
+
+    if(response.status == 201){
+        alert("Created New Supplier: " + data.companyName + ". Refreshing webpage now...")
+        window.location.reload(false)
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          margin="normal"
+          required
+          id="outlined-required"
+          label="Company Name"
+          name="companyName"
+        />
+        <br />
+        <TextField
+          fullWidth
+          margin="normal"
+          required
+          id="outlined-required"
+          label="Base Name"
+          name="base"
+        />
+        <br />
+        <br />
+        <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+          Create Supplier
+        </Button>
+      </form>
+    </div>
+  );
+}
